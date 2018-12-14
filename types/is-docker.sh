6a9c8ff449937f82eb357a83a60712e54c7c9151
Juan Cruz Viotti <juan@balena.io>
2018-12-14 17:28:28 +0000

    Add is-docker and is-npm scripts from Concourse
    Change-type: minor
    Signed-off-by: Juan Cruz Viotti <juan@balena.io>

---
+++ types/is-docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+ARGV_DIRECTORY="$1"
+set -u
+
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+"$HERE/../shared/check-dependency.sh" jq
+"$HERE/../shared/check-dependency.sh" yq
+
+cd "$ARGV_DIRECTORY"
+
+[[ -f Dockerfile ]] && exit 0
+[[ -f .resinci.yml ]] || exit 1
+
+BUILD_COUNT="$(yq r -j .resinci.yml | jq -r '.docker.builds | length')"
+[[ ${BUILD_COUNT} -gt 0 ]] && exit 0
+
+exit 1
