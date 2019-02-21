1bc0339cedcd47712a92fc27342ec0e36b890fe2
Giovanni Garufi <nazrhom@gmail.com>
2019-02-21 15:57:34 +0000

    move artefact to the correct build folder

--- node/build.sh
+++ node/build.sh
@@ -3,11 +3,11 @@ set -ex
 ARGV_DIRECTORY="$1"
 set -u
 
-cd $ARGV_DIRECTORY
+popd $ARGV_DIRECTORY
 
 version=$(jq -r '.version' package.json)
 pkg_name=$(jq -r '.name' package.json)
 
 npm pack
 
-mv ${pkg_name}-${version}.tgz build
+mv ${pkg_name}-${version}.tgz ../build
