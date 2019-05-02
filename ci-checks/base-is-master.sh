74e2beb013c8bc8b689da448006a05716a18f234
Giovanni Garufi <nazrhom@gmail.com>
2019-05-02 14:28:46 +0200

    Build PRs if a commit is marked as Backport-to
    Change-type: patch
    Signed-off-by: Giovanni Garufi <giovanni@balena.io>

--- ci-checks/base-is-master.sh
+++ ci-checks/base-is-master.sh
@@ -8,5 +8,16 @@ set -u
 
 pushd ${ARGV_DIRECTORY}
 base=$(jq -r '.base_branch' .git/.version )
+baseRepo=$(jq -r '.base_repo' .git/.version)
+baseOrg=$(jq -r '.base_org' .git/.version)
+number=$(jq -r '.number' .git/.version)
 
-[[ "${base}" == "master" ]]
+# If the PR is targetting master then we exit with success
+[[ "${base}" == "master" ]] && exit 0
+
+# Otherwise we check if a Backport footer exists, and fail if not
+COMMITS=$(find-commits parsed -r ${baseRepo} -o ${baseOrg} -n ${number})
+
+IS_BACKPORT_COMMIT=$(echo ${COMMITS} | jq -c '[.[].footers] | add' | jq 'map(select(. | startswith("Backport-to:"))) | length > 0)')
+
+[[ ${IS_BACKPORT_COMMIT} == "true" ]]
