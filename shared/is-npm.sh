70108f8cd09a9250f8e15552dbf5b1c75e7b6874
Gergely Imreh <gergely@balena.io>
2019-05-09 14:08:25 +0100

    is-npm: only run repo.yml-based checks if file exists
    Change-type: patch
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- shared/is-npm.sh
+++ shared/is-npm.sh
@@ -15,11 +15,13 @@ should_run="$(${HERE}/scripts/shared/resinci-read.sh \
 
 [[ "${should_run}" == "true" ]] && exit
 [[ "${should_run}" == "false" ]] && exit 1
-project_type="$(yq read repo.yml 'type')"
-# Explicitly check for electron packages as they appear as npm ones
-[[ "$project_type" == "electron" ]] && exit 1
-# Explicitly check for generic packages as they might appear as npm ones
-[[ "$project_type" == "generic" ]] && exit 1
+if [[ -f "repo.yml" ]]; then
+  project_type="$(yq read repo.yml 'type')"
+  # Explicitly check for electron packages as they appear as npm ones
+  [[ "$project_type" == "electron" ]] && exit 1
+  # Explicitly check for generic packages as they might appear as npm ones
+  [[ "$project_type" == "generic" ]] && exit 1
+fi
 [[ -f package.json ]] || exit 1
 
 # If we made it through the checks, exit successfully
