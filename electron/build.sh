product-os/scripts

commit a91300ef5630daf8099eca931aaa5941d9c6b1d1

Remove unused ARGV_TEMPORARY_DIRECTORY in electron scripts
Change-type: patch

electron/build.sh
@@ -31,7 +31,6 @@ usage () {
   echo "    -s <target operating system>" 1>&2
   echo "    -v <version type (production|prerelease|snapshot)>" 1>&2
   echo "    -n <npm data directory>" 1>&2
-  echo "    -w <temporary directory>" 1>&2
   exit 1
 }
 
@@ -40,7 +39,6 @@ ARGV_ARCHITECTURE=""
 ARGV_TARGET_OPERATING_SYSTEM=""
 ARGV_VERSION_TYPE=""
 ARGV_NPM_DATA_DIRECTORY=""
-ARGV_TEMPORARY_DIRECTORY=""
 
 while getopts ":b:r:s:v:n:w:" option; do
   case $option in
@@ -49,7 +47,6 @@ while getopts ":b:r:s:v:n:w:" option; do
     s) ARGV_TARGET_OPERATING_SYSTEM=$OPTARG ;;
     v) ARGV_VERSION_TYPE=$OPTARG ;;
     n) ARGV_NPM_DATA_DIRECTORY=$OPTARG ;;
-    w) ARGV_TEMPORARY_DIRECTORY=$OPTARG ;;
     *) usage ;;
   esac
 done
@@ -59,7 +56,6 @@ if [ -z "$ARGV_BASE_DIRECTORY" ] \
   || [ -z "$ARGV_TARGET_OPERATING_SYSTEM" ] \
   || [ -z "$ARGV_VERSION_TYPE" ] \
   || [ -z "$ARGV_NPM_DATA_DIRECTORY" ] \
-  || [ -z "$ARGV_TEMPORARY_DIRECTORY" ]
 then
   usage
 fi
@@ -83,6 +79,5 @@ for target in $TARGETS; do
     -r "$ARGV_ARCHITECTURE" \
     -t "$target" \
     -v "$ARGV_VERSION_TYPE" \
-    -w "$ARGV_TEMPORARY_DIRECTORY" \
     -n "$ARGV_NPM_DATA_DIRECTORY"
 done

electron/package.sh
@@ -31,7 +31,6 @@ usage () {
   echo "    -s <target operating system>" 1>&2
   echo "    -v <version type (production|prerelease|snapshot)>" 1>&2
   echo "    -n <npm data directory>" 1>&2
-  echo "    -w <temporary directory>" 1>&2
   exit 1
 }
 
@@ -40,7 +39,6 @@ ARGV_ARCHITECTURE=""
 ARGV_TARGET_OPERATING_SYSTEM=""
 ARGV_VERSION_TYPE=""
 ARGV_NPM_DATA_DIRECTORY=""
-ARGV_TEMPORARY_DIRECTORY=""
 
 while getopts ":b:r:s:v:n:w:" option; do
   case $option in
@@ -49,7 +47,6 @@ while getopts ":b:r:s:v:n:w:" option; do
     s) ARGV_TARGET_OPERATING_SYSTEM=$OPTARG ;;
     v) ARGV_VERSION_TYPE=$OPTARG ;;
     n) ARGV_NPM_DATA_DIRECTORY=$OPTARG ;;
-    w) ARGV_TEMPORARY_DIRECTORY=$OPTARG ;;
     *) usage ;;
   esac
 done
@@ -59,7 +56,6 @@ if [ -z "$ARGV_BASE_DIRECTORY" ] \
   || [ -z "$ARGV_TARGET_OPERATING_SYSTEM" ] \
   || [ -z "$ARGV_VERSION_TYPE" ] \
   || [ -z "$ARGV_NPM_DATA_DIRECTORY" ] \
-  || [ -z "$ARGV_TEMPORARY_DIRECTORY" ]
 then
   usage
 fi
@@ -83,6 +79,5 @@ for target in $TARGETS; do
     -r "$ARGV_ARCHITECTURE" \
     -t "$target" \
     -v "$ARGV_VERSION_TYPE" \
-    -w "$ARGV_TEMPORARY_DIRECTORY" \
     -n "$ARGV_NPM_DATA_DIRECTORY"
 done

electron/electron-builder.sh
@@ -30,7 +30,6 @@ usage () {
   echo "    -r <architecture>" 1>&2
   echo "    -t <package type (deb|rpm|appimage|portable|nsis|dmg|zip)>" 1>&2
   echo "    -v <version type (production|prerelease|snapshot)>" 1>&2
-  echo "    -w <temporary directory>" 1>&2
   echo "    -n <npm data directory>" 1>&2
   exit 1
 }
@@ -39,7 +38,6 @@ ARGV_BASE_DIRECTORY=""
 ARGV_ARCHITECTURE=""
 ARGV_PACKAGE_TYPE=""
 ARGV_VERSION_TYPE=""
-ARGV_TEMPORARY_DIRECTORY=""
 ARGV_NPM_DATA_DIRECTORY=""
 
 while getopts ":b:r:t:v:w:n:" option; do
@@ -48,7 +46,6 @@ while getopts ":b:r:t:v:w:n:" option; do
     r) ARGV_ARCHITECTURE=$OPTARG ;;
     t) ARGV_PACKAGE_TYPE=$OPTARG ;;
     v) ARGV_VERSION_TYPE=$OPTARG ;;
-    w) ARGV_TEMPORARY_DIRECTORY=$OPTARG ;;
     n) ARGV_NPM_DATA_DIRECTORY=$OPTARG ;;
     *) usage ;;
   esac
@@ -58,7 +55,6 @@ if [ -z "$ARGV_BASE_DIRECTORY" ] \
   || [ -z "$ARGV_ARCHITECTURE" ] \
   || [ -z "$ARGV_PACKAGE_TYPE" ] \
   || [ -z "$ARGV_VERSION_TYPE" ] \
-  || [ -z "$ARGV_TEMPORARY_DIRECTORY" ] \
   || [ -z "$ARGV_NPM_DATA_DIRECTORY" ]; then
   usage
 fi

commit 651570a5a4746b69a6f69204836b9adab76ba0be

Fix syntax errors introduced in a91300ef5630daf8099eca931aaa5941d9c6b1d1
Change-type: patch

electron/build.sh
@@ -55,7 +55,7 @@ if [ -z "$ARGV_BASE_DIRECTORY" ] \
   || [ -z "$ARGV_ARCHITECTURE" ] \
   || [ -z "$ARGV_TARGET_OPERATING_SYSTEM" ] \
   || [ -z "$ARGV_VERSION_TYPE" ] \
-  || [ -z "$ARGV_NPM_DATA_DIRECTORY" ] \
+  || [ -z "$ARGV_NPM_DATA_DIRECTORY" ]
 then
   usage
 fi

electron/package.sh
@@ -55,7 +55,7 @@ if [ -z "$ARGV_BASE_DIRECTORY" ] \
   || [ -z "$ARGV_ARCHITECTURE" ] \
   || [ -z "$ARGV_TARGET_OPERATING_SYSTEM" ] \
   || [ -z "$ARGV_VERSION_TYPE" ] \
-  || [ -z "$ARGV_NPM_DATA_DIRECTORY" ] \
+  || [ -z "$ARGV_NPM_DATA_DIRECTORY" ]
 then
   usage
 fi