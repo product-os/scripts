e81b3535aa5e6951cc0838d5745102064f93aeae
Giovanni Garufi <giovanni@balena.io>
2020-01-29 01:35:53 +0000

    Exclude VersionEvent from is-pr
    Change-type: patch
    Signed-off-by: Giovanni Garufi <giovanni@balena.io>

--- shared/is-pr.sh
+++ shared/is-pr.sh
@@ -6,6 +6,7 @@ ARGV_DIRECTORY="$1"
 
 pushd "$ARGV_DIRECTORY"
 action="$(yq r .git/.version 'action')"
+type="$(yq r .git/.version 'type')"
 
 set -o pipefail
-[[ "${action}" != "merged" ]]
+[[ "${action}" != "merged" ]] && [[ "${type}" != "VersionEvent"]]
