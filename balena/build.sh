e3499c610d5eac0109d5dd184c4440b5bce8441d
Tomás Tormo <tomast@balena.io>
2020-03-09 05:35:28 +0100

    Continue building in case a concurrent app creation happened
    Change-type: minor
    Signed-off-by: Tomás Tormo <tomast@balena.io>

--- balena/build.sh
+++ balena/build.sh
@@ -15,11 +15,35 @@ get_project_name_from_source() {
     echo "$(git -C $SRC_PATH remote -v | head -1 | sed 's|.*/\(.*\)\.git.*$|\1|g')"
 }
 
+concurrent_app_creation_happened() {
+    local app_creation_output="$1"
+    echo "$app_creation_output" | grep -q "Unique constraint violated"
+}
+
+create_balena_app() {
+  set +e
+
+  local app_name="$1"
+  local rtn=0
+
+  # If we want to get both the output and return value of a command in a subshell, we cannot use 'local'
+  # since $? will get its return value which always is... 0
+  output=$(balena app create "$app_name" -t $DEVICE_TYPE)
+  rtn=$?
+
+  if test $rtn  == 1 && concurrent_app_creation_happened "$output"; then
+      rtn=0
+  fi
+
+  set -e
+  return $rtn
+}
+
 app_name=$(get_project_name_from_source)
 
 balena login -t $API_KEY
 if ! balena_app_exists "$app_name"; then
-  balena app create $app_name -t $DEVICE_TYPE
+    create_balena_app "$app_name"
 fi
 
 balena push $app_name -s $SRC_PATH
