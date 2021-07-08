9bbbb5a39c8d17a56dc790801add25c46b96a059
Pagan Gazzard <page@balena.io>
2021-07-08 11:53:30 +0100

    Revert "Add registry mirror parameter"
    This reverts commit 55d2491120f6ec8d0a95f8c888c647cf66453417.
    
    Change-type: patch

--- build-in-container/run-build.sh
+++ build-in-container/run-build.sh
@@ -1,20 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-##############################################################
-#                                                            #
-# !!! [ShellCheck](https://www.shellcheck.net/) YOUR WORK!!! #
-#                                                            #
-#                  (please and thank you)                    #
-#                                                            #
-##############################################################
-
-
-docker_registry_mirror=${DOCKER_REGISTRY_MIRROR:-https://registry-cache-internal.balena-cloud.com}
-export docker_registry_mirror
-
-# shellcheck disable=SC1091
 source /docker-lib.sh
-start_docker "" "${docker_registry_mirror}"
+start_docker
 
 set -e
 ARGV_DIRECTORY="$1"
@@ -22,7 +9,9 @@ set -u
 
 [[ "${DEBUG}" == "false" ]] || set -x
 
-pushd "${ARGV_DIRECTORY}"
+HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+pushd $ARGV_DIRECTORY
 
 ./build-docker.sh
 
