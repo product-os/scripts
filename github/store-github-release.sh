f160ef4823ba207f48958be8585af6e99465323e
Anuj Deshpande <anuj@makerville.io>
2022-02-28 13:20:36 +0530

    Move email sending from versionbot to CI
    Add email script to github release
    Fix URL in email script
    Use branch instead of version for email subject
    Accept release url variable from resinci-deploy
    Add grep to handle output of resinci-deploy instead of return
    Get output in a separate file, then grep
    Replace curly brackets with round brackets
    Show output logs with cat
    
    Change-type: minor
    Signed-off-by: Anuj Deshpande <anuj@balena.io>

--- github/store-github-release.sh
+++ github/store-github-release.sh
@@ -24,8 +24,21 @@ ASSETS=$(find "outputs/" -type f)
 
 echo $ASSETS
 
-resinci-deploy store github-release "${ASSETS}" \
+# Example RELEASE_URL - 'https://github.com/balena-io-hardware/hardware-process-test/releases/tag/untagged-0b077a1f62efcb13db09'
+output=$(resinci-deploy store github-release "${ASSETS}" \
   -b ${headBranch} \
   -r ${repo} \
   -o ${org} \
-  -v ${version}
+  -v ${version})
+
+RELEASE_URL=$( grep -nri -A 10 'Published draft release:' <<< "$output" | grep html_url -A 1 | grep -e "[\'\"]" )
+
+SUBJECT='{"ownerName":"'${org}'", "repoName":"'${repo}'", "branch":"'${headBranch}'", "installationId":15652884}'
+BODY='Please use the release found at -'
+
+curl -s --user "api:$MAILGUN_API_KEY" \
+     https://api.mailgun.net/v3/"$MAILGUN_DOMAIN"/messages \
+	 -F from="$MAILGUN_FROM"\
+	 -F to="$MAILGUN_TO" \
+	 -F subject="$SUBJECT" \
+	 -F text="$BODY $RELEASE_URL"
